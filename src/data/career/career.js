66a6b482f5219d2fbbb8f024bb383f92a38cf164
Edberaga <edbetjonnathan@gmail.com>
2023-04-09 23:59:14 +0700

    Write the career data

---
+++ src/data/career/career.js
@@ -0,0 +1,145 @@
+import video from '../../assets/images/icons/career/video-intern.png'
+import writerIntern from '../../assets/images/icons/career/content-writer-intern.png'
+import digitalDesignerIntern from '../../assets/images/icons/career/digital-designer-intern.png'
+import web from '../../assets/images/icons/career/web-developer-intern.png'
+import accountExecutiveIntern from '../../assets/images/icons/career/account-executive-intern.png'
+
+import digitalAccount from '../../assets/images/icons/career/digital-executive.png'
+import digitalProject from '../../assets/images/icons/career/digital-project.png'
+import digitalPlanner from '../../assets/images/icons/career/digital-planner.png'
+import digitalWriter from '../../assets/images/icons/career/content-writer.png'
+import digitalDesigner from '../../assets/images/icons/career/digital-designer.png'
+
+export const careerData = [
+    {
+        title: "Video Editor Intern",
+        img: video,
+        link: '/career/internship',
+        category: "Internship",
+        content: "Working alongside a charming bunch of like-minded people to complete basic video editing works (storyboarding, shooting, editing, post production, and motion graphic)",
+    },
+    {
+        title: "Content Writer Intern",
+        img: writerIntern,
+        link: '/career/internship',
+        category: "Internship",
+        content: "Be able to learn about content writing processes with guidance from the team, and will be given the opportunity to write content regularly for social media platforms, banners and websites",
+    },
+    {
+        title: "Digital Designer Intern",
+        img: digitalDesignerIntern,
+        link: '/career/internship',
+        category: "Internship",
+        content: "Be part of the creative team and involved in conceptualising of ideas, producing storyboard and layouts, as well as creating designs for the various platforms such as online banners, website, social media postings and many more.",
+    },
+    {
+        title: "Web Developer Intern",
+        img: web,
+        link: '/career/internship',
+        category: "Internship",
+        content: "Be supporting in the development lifecycle from planning to deployment and maintenance. Core focus will be assisting on planning, coding, testing, debugging, and documentation.",
+    },
+    {
+        title: "Account Executive Intern",
+        img: accountExecutiveIntern,
+        link: '/career/internship',
+        category: "Internship",
+        content: "Be working closely with the account servicing team on handling project enquiries, assisting with research and formulating client’s proposal, and providing administrative support where necessary.",
+    },
+    {
+        title: "Digital Account Executive",
+        img: digitalAccount,
+        link: '/career/digital-account-executive',
+        category: "Fulltime",
+        content: "As a Digital Account Executive, you’ll take a hands-on approach in managing day-to-day projects, while fostering partnerships with our clients.",
+    },
+    {
+        title: "Digital Project Executive",
+        img: digitalProject,
+        link: '/career/digital-project-executive',
+        category: "Fulltime",
+        content: "As a vital member of the team, you will be working on a range of clients, liaising with different internal teams, and seeing the projects through from end to end.",
+    },
+    {
+        title: "Digital Media Planner",
+        img: digitalPlanner,
+        link: '/career/digital-media-planner',
+        category: "Fulltime",
+        content: "Digital Media Planner needs excellent communication skills, knowledge of media channels/trends, and analytical skills. This role drives innovation and cultivates key partnerships to deliver the best digital media opportunities to the client.",
+    },
+    {
+        title: "Digital Content Writer",
+        img: digitalWriter,
+        link: '/career/digital-content-writer',
+        category: "Fulltime",
+        content: "We are on the lookout for someone who is particular about spelling and grammar, highly creative, imaginative, and confident in writing about a wide range of topics.",
+    },
+    {
+        title: "Digital Designer",
+        img: digitalDesigner,
+        link: '/career/digital-designer',
+        category: "Fulltime",
+        content: "As a Digital Designer, you will be given countless opportunities to showcase your talents, such as designing and developing rich front-end interfaces, websites, and rich media advertising that will wow consumers and help brands to grow.",
+    },
+    {
+        title: "Video Grapher",
+        img: digitalDesigner,
+        link: '/career/video-grapher',
+        category: "Fulltime",
+        content: "Say Sheji Group Sdn. Bhd. is a dynamic and rapidly growing company that offers a wide range of creative and innovative solutions to our clients. Our company is committed to providing exceptional customer service and we are dedicated to the professional growth and development of our employees. We are seeking a talented Video Editor to join our ThaiGood4U YouTube Channel team in Thailand.",
+    },
+    {
+        title: "Brand Ambassador",
+        img: digitalProject,
+        link: '/career/brand-ambassador',
+        category: "Fulltime",
+        content: "Say Sheji Group Sdn. Bhd. is a dynamic and rapidly growing company that offers a wide range of creative and innovative solutions to our clients. Our company is committed to providing exceptional customer service and we are dedicated to the professional growth and development of our employees. We are seeking a talented Brand Ambassador to represent our ThaiGood4U YouTube Channel.",
+    },
+    {
+        title: "Junior Graphic Designer",
+        img: digitalPlanner,
+        link: '/career/junior-graphic-designer',
+        category: "Fulltime",
+        content: "Say Sheji Group Sdn. Bhd. is a dynamic and rapidly growing company that offers a wide range of creative and innovative solutions to our clients. Our company is committed to providing exceptional customer service and we are dedicated to the professional growth and development of our employees. We are seeking a talented Junior Graphic Designer to join our ThaiGood4U YouTube Channel team in Thailand.",
+    },
+    {
+        title: "Video Editor",
+        img: digitalWriter,
+        link: '/career/video-editor',
+        category: "Fulltime",
+        content: "Say Sheji Group Sdn. Bhd. is a dynamic and rapidly growing company that offers a wide range of creative and innovative solutions to our clients. Our company is committed to providing exceptional customer service and we are dedicated to the professional growth and development of our employees. We are seeking a talented Video Editor to join our ThaiGood4U YouTube Channel team in Thailand.",
+    },
+]
+
+export const InternshipData = [
+    {
+        title: "Digital Content Writer",
+        video: ConWriVid,
+        content: "You will be able to understand the content writing process under the guidance of the team and have the opportunity to regularly write content for social media platforms, banners and websites.",
+        require: "You will be exposed to a wide range of topics and industries, and gain knowledge about the latest advertising trends and dynamics in the digital industry. Proficiency in multiple languages will be a plus."
+    },
+    {
+        title: "Digital Designer",
+        video: DigDesVid,
+        content: "You will become a member of the creative team and participate in the conceptualization of concepts, making plot synopses and layouts, and creating designs for various platforms (for example, online banners, websites, social media postings, etc).",
+        require: "You need to be proficient in Adobe Photoshop, Illustrator, and if you have other relevant knowledge (such as HTML5 or CSS), then this will be a bonus."
+    },
+    {
+        title: "Account Executive",
+        video: AccExeVid,
+        content: "You will work closely with the customer service team to handle project enquiries, assist in researching and formulating customer recommendations, and provide administrative support when necessary.",
+        require: "You will be able to learn how to build relationships with customers and suppliers, as well as develop organizational and project management skills."
+    },
+    {
+        title: "Video Editor",
+        video: VidEdiVid,
+        content: "You will complete basic video editing work (drawing board, shooting, editing, post-production and motion graphics) with a group of like-minded people.",
+        require: "In addition, you will also be exposed to many brands from different industries. If you are passionate about making short videos, dynamic graphics, and engaging social content, it's perfect for the role."
+    },
+    {
+        title: "Web Developer",
+        video: WebDevVid,
+        content: "You will be supporting in the development lifecycle from planning to deployment and maintenance.",
+        require: "Core focus will be assisting on planning, coding, testing, debugging, and documentation. If you posses any relevant experience in PHP, ASP.NET, HTML, CSS, JavaScript, and cross browser implementations, it will be a plus."
+    },
+]
